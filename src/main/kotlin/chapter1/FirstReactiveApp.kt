8e627ef0f384235a400478b007722f1607443023
Zu Da Ng <zuda@m-daq.com>
2018-01-14 19:14:45 +0800

    First reactive program

---
+++ src/main/kotlin/chapter1/FirstReactiveApp.kt
@@ -0,0 +1,20 @@
+package chapter1
+
+import io.reactivex.Observable
+import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.rxkotlin.toObservable
+
+
+fun main(args: Array<String>) {
+    val list: List<Any> = listOf("One", 2, "Three", "Four", 4.5, "Five", 6.0f)
+    val observable: Observable<Any> = list.toObservable()
+
+    observable.subscribeBy(
+            onNext = { println(it) },
+            onError = { it.printStackTrace() },
+            onComplete = { println("Done!") })
+}
+
+
+
+
